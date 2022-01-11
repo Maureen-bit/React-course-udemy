6068eb37ad16cb0f8a2a3f4a8ab867a3551d6aeb
Maureen Parra Mondragón <mauparra@publicisgroupe.net>
2022-01-11 17:56:42 -0500

    React start

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,8 @@
+// importar react ayuda a solucionar el error de: "Jsx must be in a react scope"
+import React from 'react';
+
+const PrimeraApp = () => {
+    return <h1>Hola Maureen!</h1>;
+};
+
+export default PrimeraApp;
